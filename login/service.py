fc8dfb7007fabda52534320323333dade56b2604
Marcos Temple <marcostemple23@gmail.com>
2024-09-28 15:22:47 -0300

    repository service pattern

--- login/service.py
+++ login/service.py
@@ -8,3 +8,13 @@ def login(username, password):
         username=username,
         password=password,
     )
+
+    if response.get("error"):
+        st.error(f'Falha ao realizar login: {response.get('error')}')
+    else:
+        st.session_state.token = response.get('access')
+        st.rerun()
+
+def logout():
+    for key in st.session_state.keys():
+        del st.session_state[key]
